73b25cd7f0f3abe8884cc0abc5d3978eafe89b8d
Chendi.Xue <chendi.xue@intel.com>
2024-10-14 18:55:54 +0000

    add a print for first next token latency
    Signed-off-by: Chendi.Xue <chendi.xue@intel.com>

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -578,6 +578,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
         self.prompt_adapter_config = prompt_adapter_config
         self.return_hidden_states = return_hidden_states
         self.observability_config = observability_config
+        self.sampling_time = {}
 
         self.sliding_window = (model_config.get_sliding_window()
                                if model_config is not None else None)
@@ -1183,6 +1184,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
                                     dtype=self.model_config.dtype,
                                     device=self.device)
 
+        #print(f"block_mapping shape is ", block_mapping.shape, "content is ", block_mapping)
         attn_metadata = self.attn_backend.make_metadata(
             is_prompt=False,
             block_list=block_list,
@@ -1266,6 +1268,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
             slot_mapping,
             lora_ids,
         ) = self._prepare_prompt(prefill_reqs)
+        start = time.perf_counter()
         (
             decode_input_tokens,
             decode_input_positions,
@@ -1352,6 +1355,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
             "seq_lens": seq_lens,
             "query_lens": query_lens
         }
+
         if prefill_attn_metadata is not None:
             metadata_dict.update(prefill_attn_metadata.asdict_zerocopy())
         else:
@@ -2123,6 +2127,7 @@ class HPUModelRunner(HPUModelRunnerBase[ModelInputForHPUWithSamplingMetadata]):
                                 data.output_token_ids[:orig_output_tokens_len]
 
             for i in range(num_steps):
+                start = time.perf_counter()
                 with self.profiler.record_event('internal', model_event_name):
                     hidden_states = self.model.forward(
                         **execute_model_kwargs,
@@ -2206,6 +2211,11 @@ class HPUModelRunner(HPUModelRunnerBase[ModelInputForHPUWithSamplingMetadata]):
                     })
                 else:
                     try_revert_dummy_output_tokens()
+                if not is_prompt:
+                    #htorch.hpu.synchronize()
+                    if batch_size not in self.sampling_time:
+                        self.sampling_time[batch_size] = []
+                    self.sampling_time[batch_size].append(time.perf_counter() - start)
 
             if self.is_driver_worker and self.profiler.enabled:
                 # Stop recording 'execute_model' event
@@ -2288,6 +2298,20 @@ class HPUModelRunner(HPUModelRunnerBase[ModelInputForHPUWithSamplingMetadata]):
                 finalize_calibration)
             finalize_calibration(self.model.model)
             self._is_inc_finalized = True
+    
+    def print_perf(self):
+        try:
+            sampling_statistic = [f"fwd + logits + sampling: bs == {bs}, avg_time is {sum(v)/len(v) * 1000} msecs, counts is {len(v)}\n" for bs, v in self.sampling_time.items()]  # noqa: E501
+            #sampling_statistic = [f"fwd + logits + sampling: {self.sampling_time}"]  # noqa: E501
+        except:
+            sampling_statistic = []
+        print(sampling_statistic)
+
+    def reset_perf(self):
+        self.sampling_time = {}
 
     def __del__(self):
-        self.shutdown_inc()
+        try:
+            self.shutdown_inc()
+        except:
+            pass
