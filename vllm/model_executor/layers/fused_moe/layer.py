e85e38c7a23c5222f0a8863aa896c658b0b27732
Kunshang Ji <kunshang.ji@intel.com>
2025-04-07 05:20:18 +0000

    fix OOM issue

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -667,8 +667,7 @@ class FusedMoE(torch.nn.Module):
                                                  shard_size)
         # w2, down_proj: Load into only logical weight of w2.
         expert_data.copy_(loaded_weight)
-        # if is_hpu:
-        #     self.hpu_fused_moe.MoeOp.w2_list[expert_id].set_weight(expert_data)
+        torch.hpu.synchronize()
 
     def _load_single_value(self, param: torch.nn.Parameter,
                            loaded_weight: torch.Tensor, expert_id: int):
