96dc7f98fca8bfcdc52ddcfe73e2abec3ed50e8d
Qiming Zhang <qiming1.zhang@intel.com>
2025-07-03 21:57:39 -0700

    fix gptq bias (#240)

--- vllm/model_executor/layers/quantization/ipex_quant.py
+++ vllm/model_executor/layers/quantization/ipex_quant.py
@@ -224,6 +224,8 @@ class IPEXGPTQLinearMethod(GPTQLinearMethod):
         self.quant_config = quant_config  # type: ignore
 
     def process_weights_after_loading(self, layer: torch.nn.Module) -> None:
+        bias = layer.bias if not layer.skip_bias_add else None
+
         try:
             import intel_extension_for_pytorch as ipex
             if ipex.__version__ < MIN_IPEX_VERSION:
@@ -265,7 +267,7 @@ class IPEXGPTQLinearMethod(GPTQLinearMethod):
             layer.ipex_output_size,
             qconfig=qconfig,
             g_idx=g_idx,
-            bias=None,
+            bias=bias,
             group_size=self.quant_config.group_size,
             quant_method=IPEXConfig.IPEX_QUANT_METHOD_MAP["gptq"],
             weight_qscheme="sym" if self.quant_config.is_qweight_sym else "asym",
