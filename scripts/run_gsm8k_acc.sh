1ce241df2f30755ff19d20cedd5e221db5555bad
Chendi Xue <chendi.xue@intel.com>
2025-07-18 00:50:31 +0000

    Add TP script
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>
    Signed-off-by: root <Chendi Xue>

--- scripts/run_gsm8k_acc.sh
+++ scripts/run_gsm8k_acc.sh
@@ -5,6 +5,6 @@ TP_SIZE=1
 
 VLLM_USE_V1=1 \
 lm_eval --model vllm \
-  --model_args "pretrained=${MODEL_PATH},tensor_parallel_size=${TP_SIZE},trust_remote_code=true,max_model_len=4096" \
+  --model_args "pretrained=${MODEL_PATH},tensor_parallel_size=${TP_SIZE},trust_remote_code=true,max_model_len=4096,dtype=float16" \
   --tasks gsm8k --num_fewshot "5" \
   --batch_size "auto" --log_samples --output_path gsm8k_acc_${MODEL_NAME}.json
